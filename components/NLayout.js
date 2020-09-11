1a427049f26257930fe701b73b840dc29470a553
gabrielSBorges <borgesgabrielsilva@gmail.com>
2020-09-11 01:00:36 -0300

    Configurações iniciais no layout da página. Get de marcas e modelos ok.

---
+++ components/NLayout.js
@@ -0,0 +1,21 @@
+export default {
+  template: /*html*/`
+    <div>
+      <v-app-bar app clipped-left dense id="header">
+        <span class="primary--text ml-3">Navita</span>
+      </v-app-bar>
+  
+      <v-main>
+        <v-container>
+          <slot />
+        </v-container>
+      </v-main>
+  
+      <v-footer app padless>
+        <v-card flat tile class="flex text-center">
+          <v-card-text>Copyright &copy Navita 2020</v-card-text>
+        </v-card>
+      </v-footer>
+    </div>
+  `
+}
